ff34a0d4ca93b6cdfcbe3e65b3f0372a18f08f8d
fbnGametor <trindadefabio2016@gmail.com>
2026-04-13 14:09:37 -0300

    init flashcards project

---
+++ api/src/main/java/com/fabio/flashcards_app/FlashcardsAppApplication.java
@@ -0,0 +1,13 @@
+package com.fabio.flashcards_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashcardsAppApplication.class, args);
+	}
+
+}
